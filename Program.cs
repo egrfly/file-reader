5232a154f4418566b068ecafb7221c13eaea2151
egrfly <egrfly@outlook.com>
2024-02-26 09:17:59 +0000

    Add file opening example

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter the name of the file you wish to read from: ");
+var fileName = Console.ReadLine() ?? "";
+try
+{
+    var fileReader = new StreamReader(fileName);
+    Console.WriteLine("Here is the content of your file:");
+    Console.WriteLine(fileReader.ReadToEnd().Trim());
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("Sorry, that file was not found.");
+}
